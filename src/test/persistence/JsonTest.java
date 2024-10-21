4cdb25154a10d0b58656975171facde45362261e
“kanwx” <kanwx@students.cs.ubc.ca>
2024-10-21 10:45:00 -0700

    Add Json Files

---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,23 @@
+package persistence;
+
+import model.*;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonTest {
+    protected void checkCar(String name, String type, String color, int year, int miles, int hp, int price, Car car){
+        assertEquals(name, car.getName());
+        assertEquals(type, car.getType());
+        assertEquals(color, car.getColor());
+        assertEquals(year, car.getYear());
+        assertEquals(miles, car.getMiles());
+        assertEquals(hp, car.getHp());
+        assertEquals(price, car.getPrice());
+    }
+
+    protected void checkGarage(String garageName, ArrayList<Car> cars, Garage garage){
+        assertEquals(garageName, garage.getName());
+        
+
+    }
+}
