3c85e2163445297355afde733cc50842fa945f1f
“kanwx” <kanwx@students.cs.ubc.ca>
2024-10-21 23:28:23 -0700

    Add JsonReaderTest

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class JsonReaderTest extends JsonTest {
 
-    //EFFECTS: Test the reader when there is no existent file
+    // EFFECTS: Test the reader when there is no existent file
     @Test
     void testReaderNonExistentFile() {
         JsonReader reader = new JsonReader("./data/noSuchFile.json");
@@ -23,7 +23,7 @@ class JsonReaderTest extends JsonTest {
         }
     }
 
-    //EFFECTS: Test the reader when there is an empty collection
+    // EFFECTS: Test the reader when there is an empty collection
     @Test
     void testReaderEmptyCollection() {
         JsonReader reader = new JsonReader("./data/testReaderEmptyCollection.json");
@@ -36,14 +36,26 @@ class JsonReaderTest extends JsonTest {
         }
     }
 
-   @Test
+    @Test
     void testReaderGeneralLibrary() {
         JsonReader reader = new JsonReader("./data/testReaderCollection.json");
         try {
             Collection collection = reader.read();
             List<Car> cars = collection.getCars();
             List<Garage> garages = collection.getSavedGarages();
-            assertEquals(5, cars.size());
+            ArrayList<Car> testGarage1 = new ArrayList<>();
+
+            assertEquals(4, cars.size());
+            checkCar("Porsche GT3 RS", "racing car", "white and red", 2024, 2500, 518, 328888, "Prw2500", cars.get(0));
+            checkCar("Ferrari SF90", "hyper car", "red", 2020, 40000, 986, 698888, "Fhr40000", cars.get(1));
+            checkCar("McLaren P1", "hyper car", "yellow", 2014, 100, 916, 1350000, "Mhy100", cars.get(2));
+            checkCar("Maybach S680", "sedan", "black", 2023, 5000, 621, 349597, "Msb5000", cars.get(3));
+
+            testGarage1.add(cars.get(0));
+            testGarage1.add(cars.get(3));
+            assertEquals(2, garages.size());
+
+            checkGarage("racer's favorite", testGarage1, garages.get(0));
         } catch (IOException e) {
             fail("Couldn't read from file");
         }
--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -5,7 +5,8 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class JsonTest {
-    protected void checkCar(String name, String type, String color, int year, int miles, int hp, int price, Car car) {
+    protected void checkCar(String name, String type, String color, int year, int miles, int hp, int price, String id,
+            Car car) {
         assertEquals(name, car.getName());
         assertEquals(type, car.getType());
         assertEquals(color, car.getColor());
@@ -13,14 +14,16 @@ public class JsonTest {
         assertEquals(miles, car.getMiles());
         assertEquals(hp, car.getHp());
         assertEquals(price, car.getPrice());
+        assertEquals(id, car.getId());
     }
 
-    protected void checkGarage(String garageName, ArrayList<Car> cars, Garage garage){
+    protected void checkGarage(String garageName, ArrayList<Car> cars, Garage garage) {
         assertEquals(garageName, garage.getName());
         int index = 0;
         for (Car car : cars) {
-            checkCar(car.getName(), car.getType(), car.getColor(), car.getYear(), car.getMiles(),car.getHp(),car.getPrice(),
-            garage.getCars().get(index));
+            checkCar(car.getName(), car.getType(), car.getColor(), car.getYear(), car.getMiles(), car.getHp(),
+                    car.getPrice(), car.getId(),
+                    garage.getCars().get(index));
             index++;
         }
 
